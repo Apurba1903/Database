702ae24c7a8b193a33c1d717ef7869994def5348
Apurba1903 <apurba1903@gmail.com>
2025-06-23 00:22:07 +0600

    questions and dataset added

---
+++ SQL/Flights/flights_datetime_case_study.sql
@@ -0,0 +1,114 @@
+USE flightsdb;
+
+SELECT * FROM flights;
+
+# 1. Find the month with the most number of flights
+
+
+
+
+# 2. Which weekday has the most costly flights
+
+
+
+
+# 3. Find the number of Indigo flights every month
+
+
+
+
+# 4. Find the list of all flights that depart between 10 AM and 2 PM from Delhi to Bangalore
+
+
+
+
+# 5. Find the number of flights departing on weekends from Bangalore
+
+
+
+
+# 6. Calculate the arrival time for all flights by adding the duration to the departure time
+
+
+
+
+
+# 7. Calculate the arrival date for all the flights
+
+
+
+
+
+# 8. Find the number of flights which travel on multiple dates
+
+
+
+
+
+# 9. Calculate the average duration of flights between all city pairs (output in `xh ym` format)
+
+
+
+
+
+# 10. Find all flights which departed before midnight but arrived after midnight with 0 stops
+
+
+
+
+
+# 11. Find quarter-wise number of flights for each airline
+
+
+
+
+
+# 12. Find the longest flight distance (between cities in terms of time) in India
+
+
+
+
+
+# 13. Calculate the average time duration for flights with 1 stop vs more than 1 stop
+
+
+
+
+
+# 14. Find all Air India flights in a given date range originating from Delhi
+
+
+
+
+
+# 15. Find the longest flight of each airline
+
+
+
+
+
+# 16. Find all pairs of cities having average flight duration greater than 3 hours
+
+
+
+
+
+# 17. Make a weekday vs time grid showing frequency of flights from Bangalore and Delhi
+
+
+
+
+
+
+# 18. Make a weekday vs time grid showing average flight price from Bangalore and Delhi
+
+
+
+
+
+
+
+
+
+
+
